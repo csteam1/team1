7090fc268f5ed1613f5f64b39b498859789bb63d
Rushabh <rushabh14@gmail.com>
2017-07-11 05:23:13 -0700

    WebController.java UserRepository.java

--- src/main/java/com/team1/controller/WebController.java
+++ src/main/java/com/team1/controller/WebController.java
@@ -1,20 +1,29 @@
 package com.team1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team1.data.User;
+import com.team1.services.UserRepository;
+
 
 @RestController
 public class WebController {
 
 	@Autowired
+	UserRepository repo;
+	@Autowired
 	
 	
-	@RequestMapping("/register")
-	public String registerUser ()
-	{
-		return "User Created";
+	@RequestMapping(value = "/register", method=RequestMethod.POST, consumes= MediaType.APPLICATION_JSON_VALUE)
+	public String registerUser(@RequestBody User user){
+		return repo.register(user);
+		
 	}
 	
 	@RequestMapping("/user/all")
--- src/main/java/com/team1/services/UserRepository.java
+++ src/main/java/com/team1/services/UserRepository.java
@@ -1,5 +1,69 @@
 package com.team1.services;
 
-public class UserRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import com.team1.data.User;
+
+
+public class UserRepository {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public String register (final User user){
+		
+		final String checkSql = "select * from user where name=? and email=?";
+		try{
+			User tempUser = jdbcTemplate.queryForObject(checkSql, new Object[]{user.getUser(), user.getMail()}, new UserRowMapper());
+			
+			return "User already exists!!!";
+		
+		}catch(EmptyResultDataAccessException e){
+			
+		
+		
+			final String sql = "insert into user(name,email) values(?,?)";
+			KeyHolder holder = new GeneratedKeyHolder();
+			jdbcTemplate.update(new PreparedStatementCreator(){
+				@Override
+				public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+					PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+					ps.setString(1, user.getUser());
+					ps.setString(2, user.getMail());
+					return ps;
+				}
+			}, holder);
+			
+			int newUserId = holder.getKey().intValue();
+			user.setId(newUserId);
+			return "Your User ID: "+ user.getId();
+		
+		
+		}
+	}
+}
+
+
+class UserRowMapper implements RowMapper<User>
+{
+	@Override
+	public User mapRow(ResultSet rs, int rowNum) throws SQLException{
+		User user = new User();
+		user.setUser(rs.getString("name"));
+		user.setMail(rs.getString("email"));
+		return user;
+	}
 }
