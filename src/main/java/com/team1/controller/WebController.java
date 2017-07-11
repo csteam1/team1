6a35e4b6200b8cf5c604e63fb845bf4586998c7e
Chinmay <chinmaykarmalkar@yahoo.in>
2017-07-11 04:25:35 -0700

    Updated controller file

--- src/main/java/com/team1/controller/WebController.java
+++ src/main/java/com/team1/controller/WebController.java
@@ -1,5 +1,45 @@
 package com.team1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebController {
 
+	@Autowired
+	
+	
+	@RequestMapping("/register")
+	public String registerUser ()
+	{
+		return "User Created";
+	}
+	
+	@RequestMapping("/user/all")
+	public String detailsOfAllUsers ()
+	{
+		return "Detail of users";
+	}
+	
+	@RequestMapping("/user/{userId}")
+	public String detailOfSingleUser(@PathVariable int userId)
+	{
+		return "User Created userId = " + userId;
+	}
+	
+	@RequestMapping("/order")
+	public String placeOrder ()
+	{
+		return "Order placed";
+	}
+	
+	@RequestMapping("/cancelOrder")
+	public String cancelOrder ()
+	{
+		return "Cancel order requested";
+	}
+	
+	
 }
--- src/main/java/com/team1/cs/CsApplication.java
+++ src/main/java/com/team1/cs/CsApplication.java
@@ -2,8 +2,11 @@ package com.team1.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("com.team1")
 public class CsApplication {
 
 	public static void main(String[] args) {
