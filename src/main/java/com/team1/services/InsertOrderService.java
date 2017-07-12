038f74bfb43ee857b6fbce723f04a6cc23e8bb22
Your Name <you@example.com>
2017-07-12 01:18:26 -0700

    Add dates and modify the query

--- src/main/java/com/team1/services/InsertOrderService.java
+++ src/main/java/com/team1/services/InsertOrderService.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,28 +29,37 @@ public class InsertOrderService {
 	public String insertOrder(Order order){
 		//final String checkSql = "insert into transaction() ";
 		final String sql = "insert into "
-				+ "transaction(u_id, t_id, side, typeOrder, "
+				+ "transaction(u_id, t_id, typeOrder, side, "
 				+ "lotSize, dateOfTransaction, price, currencyFrom, "
 				+ "currencyTo, status, limitPrice) "
 				+ "values(?,?,?,?,?,?,?,?,?,?,?)";
 		
 		KeyHolder holder = new GeneratedKeyHolder();
 		jdbcTemplate.update(new PreparedStatementCreator() {
-			
 			@Override
 			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
 				// TODO Auto-generated method stub
 				PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 				ps.setInt(1, order.getU_id());
-				ps.setInt(2, 0);
+				
+				Random rand = new Random();
+				int transId = rand.nextInt(32767) + 1000;
+				ps.setInt(2, transId);
+				order.setT_id(transId);
+				
 				ps.setString(3, order.getTypeOrder().name());
 				ps.setString(4, order.getSide().name());
 				ps.setInt(5, order.getLotSize());
-				ps.setString(6, "dummy");
+				
+				DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				Date date = new Date();
+				ps.setString(6, dateFormat.format(date));
+				
 				ps.setDouble(7, 0);
 				ps.setString(8, order.getCurrencyFrom().name());
 				ps.setString(9, order.getCurrencyTo().name());
 				ps.setString(10, Status.NOT_COMPLETED.name());
+				
 				if(order.getTypeOrder() == Type.MO)
 					ps.setDouble(11, 0);
 				else
@@ -54,7 +67,6 @@ public class InsertOrderService {
 				return ps;
 			}
 		}, holder);
-		order.setT_id(0);
 		return "Success";
 	}
 }
