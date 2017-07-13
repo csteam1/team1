29ce927e344bca80f7b2d208eda9dcff9e0c63e1
Chinmay <chinmaykarmalkar@yahoo.in>
2017-07-12 21:47:48 -0700

    Updated validation.java

--- src/main/java/com/team1/validation/Validation.java
+++ src/main/java/com/team1/validation/Validation.java
@@ -1,33 +1,57 @@
 package com.team1.validation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
 import com.team1.data.Currency;
 import com.team1.data.Order;
 import com.team1.data.Side;
 import com.team1.data.Status;
 import com.team1.data.Type;
+import com.team1.data.User;
 
 public class Validation {
 	
-	private Currency currencyFrom, currencyTo;
+	private String currencyFrom, currencyTo;
     private String side;
-    private int u_id, t_id;
-    private String date;
-    private double price;
-    private Type type;
+    private int uid, t_id;
+    private String type;
     private int lotSize;
-    private Status status;
-	
+    private double limitPrice;
+    
+    
+    @Autowired
+	private JdbcTemplate jdbcTemplate;
+    
 	public String validateOrder (Order order)
 	{
 		//Cheking userID from database pending
+				
+		uid = order.getU_id();
 		
-		currencyFrom = order.getCurrencyFrom();
-		currencyTo = order.getCurrencyTo();
-		side = order.getSide().name();
+				
+		lotSize = order.getLotSize();
+		if (lotSize < 1)
+			return "Lot size should be greater than 1";
 		
-	    
 		
-		return "false";
+		if (type == Type.LO.name())
+		{
+			limitPrice = order.getLimitPrice();
+			if (limitPrice < 1)
+				return "Please enter limit price";
+		} else {
+			
+		}
+			
+		return "success";
 		
 	}
 
